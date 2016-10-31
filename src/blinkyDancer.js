04b4b9a1628a3727b3f75eda8c39b3c191c60d2a
MKS-students <mks.students@hackreactor.com>
2016-10-31 18:31:21 -0500

    Done with reimplementing classes as pseudoclassical.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,41 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+//var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
+  //var oldStep = blinkyDancer.step;
 
-  blinkyDancer.step = function() {
+  //blinkyDancer.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
+    //oldStep();
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+    //blinkyDancer.$node.toggle();
+  //};
 
-  return blinkyDancer;
+  //return blinkyDancer;
+//};
+
+//Pseudo-classical implementation of BlinkyDancer class.
+var BlinkyDancer = function(top,left, timeBetweenSteps) {
+  //console.log(Dancer)
+  Dancer.call(this,top,left,timeBetweenSteps);
+  this.step();
+  //onsole.log("oldstep:"+this.oldStep);
 };
+
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.oldStep = Dancer.prototype.step;
+
+//Pseudo-classical implementation of methods.
+BlinkyDancer.prototype.step = function() {
+  console.log("oldstep:"+this.oldStep);
+  console.log("this.$node:"+this.$node);
+  this.oldStep();
+  this.$node.toggle();
+};
+
+//Set up inheritance
+BlinkyDancer.prototype.constructor = BlinkyDancer;
